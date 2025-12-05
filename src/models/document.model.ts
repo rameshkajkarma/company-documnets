731d76079e1d00fa58e3a2a6597212fdfa2e2553
Ramesh Sura <suraramesh46@gmail.com>
2025-12-05 13:58:14 +0530

    Updated license controller with pagination and status

--- src/models/document.model.ts
+++ src/models/document.model.ts
@@ -1,29 +1,3 @@
-// import { Schema, model } from "mongoose";
-
-// export const allowedDocumentCategories = [
-//   "Contract",
-//   "Template",
-//   "Agreement",
-//   "Policy",
-//   "Other"
-// ];
-
-// const documentSchema = new Schema(
-//   {
-//     name: { type: String, required: true },
-//     category: { 
-//       type: String, 
-//       required: true,
-//       enum: allowedDocumentCategories   // ✅ restrict to only these
-//     },
-//     documentDate: { type: Date, required: true },
-//     partiesInvolved: { type: String, required: true },
-//     fileKey: { type: String }
-//   },
-//   { timestamps: true }
-// );
-
-// export default model("Document", documentSchema);
 
 import { Schema, model } from "mongoose";
 
